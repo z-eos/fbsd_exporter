9ee5b0dc5f10e32583a51e52f2d1f70b8d2a51a3
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2026-01-23 13:37:45 +0200

    cpu temperature collecting added; per-cpu data splitted to separate settings in config file

--- lib/cpu.sh
+++ lib/cpu.sh
@@ -9,13 +9,13 @@ collect_cpu() {
     ncpu=$(sysctl -n hw.ncpu 2>/dev/null || echo 1)
     hz=$(sysctl -n kern.clockrate 2>/dev/null | sed -n 's/.*[,{][[:space:]]*hz[[:space:]]*=[[:space:]]*\([0-9][0-9]*\).*/\1/p' || echo 128)
 
-    if [ "$ENABLE_CPU_PERCPU" = "1" ]; then
+    ################
+    # per-CPU time #
+    ################
+    if [ "$ENABLE_CPU_PERCPU_TIME" = "1" ]; then
 	metric_help "${METRIC_NAME_PREFIX}_cpu_percpu_time_seconds_total" "per-CPU time in seconds"
 	metric_type "${METRIC_NAME_PREFIX}_cpu_percpu_time_seconds_total" "counter"
-
-	# Get per-CPU stats using sysctl
 	# kern.cp_times: user, nice, system, interrupt, idle per CPU
-
 	sysctl -n kern.cp_times 2>/dev/null | _awk -v ncpu="$ncpu" -v hz="$hz" '
     BEGIN {
 	split("user nice system interrupt idle", states)
@@ -35,6 +35,20 @@ collect_cpu() {
     }'
     fi
 
+    #######################
+    # per-CPU temperature #
+    #######################
+    if [ "$ENABLE_CPU_PERCPU_TEMPERATURE" = "1" ]; then
+	metric_help "${METRIC_NAME_PREFIX}_cpu_percpu_temperature_total" "per-CPU time in seconds"
+	metric_type "${METRIC_NAME_PREFIX}_cpu_percpu_temperature_total" "gauge"
+
+	# Get per-CPU temperature
+	for i in `seq 0 $ncpu`; do
+	    t=$(sysctl -n dev.cpu.$i.temperature)
+	    metric "${METRIC_NAME_PREFIX}_cpu_percpu_temperature_total" "cpu=\"$i\"" "${t%?}"
+	done
+    fi
+
     metric_help "${METRIC_NAME_PREFIX}_cpu_time_total" "CPU time in percents"
     metric_type "${METRIC_NAME_PREFIX}_cpu_time_total" "gauge"
 
@@ -51,7 +65,9 @@ collect_cpu() {
 	printf "%s_cpu_time_total{mode=\"idle\"} %.2f\n", pfx, $5 / total * 100
     }'
 
-    # Load averages
+    #################
+    # Load averages #
+    #################
     metric_help "${METRIC_NAME_PREFIX}_sys_loadavg" "System load average"
     metric_type "${METRIC_NAME_PREFIX}_sys_loadavg" "gauge"
 
@@ -61,7 +77,9 @@ collect_cpu() {
 	printf "%s_sys_loadavg{period=\"15m\"} %s\n", pfx, $4
     }'
 
-    # Context switches and interrupts
+    ###################################
+    # Context switches and interrupts #
+    ###################################
     metric_help "${METRIC_NAME_PREFIX}_sys_context_switches_total" "Total context switches"
     metric_type "${METRIC_NAME_PREFIX}_sys_context_switches_total" "counter"
     vm_swtch=$(sysctl -n vm.stats.sys.v_swtch 2>/dev/null || echo 0)
@@ -87,7 +105,9 @@ collect_cpu() {
     vm_soft=$(sysctl -n vm.stats.sys.v_soft 2>/dev/null || echo 0)
     metric "${METRIC_NAME_PREFIX}_sys_interrupts_soft_total" "" "$vm_soft"
 
-    # CPU count
+    #############
+    # CPU count #
+    #############
     metric_help "${METRIC_NAME_PREFIX}_sys_cpu_count" "Number of CPUs"
     metric_type "${METRIC_NAME_PREFIX}_sys_cpu_count" "gauge"
     metric "${METRIC_NAME_PREFIX}_sys_cpu_count" "" "$ncpu"
